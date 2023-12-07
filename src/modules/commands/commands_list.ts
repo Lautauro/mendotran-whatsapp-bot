2885422cd8c2075d7c451f81db352d35c848da37
Lautauro <aurolaut@protonmail.com>
2023-12-07 20:18:29 -0300

    Cleaning commands

--- src/modules/commands/commands_list.ts
+++ src/modules/commands/commands_list.ts
@@ -1,5 +1,4 @@
-import { MessageMedia } from "whatsapp-web.js";
-import { command_example, createCommand, search_command, send_error_response, send_response } from "./commands.js";
+import { createCommand, send_response } from "./commands.js";
 
 /**
  * Basic commands
@@ -26,26 +25,3 @@ createCommand(['pong'],
         description: 'Pong-ping! 🏓',
     })
 .closeCommand()
-
-// Quoted message example:
-createCommand(['quote', 'cite'],
-    async (args, message) => {
-        message.getQuotedMessage().then((quotedMessage) => {
-            if (quotedMessage.type === 'chat' || (quotedMessage.type === 'image' && quotedMessage.body.length)) {
-                send_response(
-                    `*" ${quotedMessage.body} "*\n\n` + 
-                    // @ts-ignore
-                    `- _${quotedMessage._data.notifyName}_`,
-                    message
-                );
-            }
-        })
-    },
-    {
-        needQuotedMessage: true
-    },
-    {
-        name: 'Quote this',
-        description: 'This command makes an author quote with the selected message. It needs to quote a message to work.'
-    })
-.closeCommand();
