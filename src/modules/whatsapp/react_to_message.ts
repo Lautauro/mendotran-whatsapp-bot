3f9038d1cfd8314c456716ca1a0eb28bdeca6dd7
Lautauro <aurolaut@protonmail.com>
2023-12-08 17:58:58 -0300

    Clear reaction

--- src/modules/whatsapp/react_to_message.ts
+++ src/modules/whatsapp/react_to_message.ts
@@ -1,5 +1,6 @@
 import { Message } from "whatsapp-web.js";
 
 export async function react_to_message(reaction: string, message: Message): Promise<void> {
+    await message.react(''); // Clear reaction
     return message.react(reaction);
 }
