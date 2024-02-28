5e837984f46dfcd33d64d2c27a48c81f818e872d
Lautauro <aurolaut@protonmail.com>
2024-02-28 16:49:38 -0300

    Updated "CommandCallback"

--- src/ts/types/commands.d.ts
+++ src/ts/types/commands.d.ts
@@ -1,4 +1,4 @@
 import { Message } from "whatsapp-web.js";
 
 export type ParameterType = 'string' | 'number' | 'boolean' | 'any';
-export type CommandCallback = (args: any[], message: Message) => void;
+export type CommandCallback = (args: any[], message: Message) => void | Promise<void>;
