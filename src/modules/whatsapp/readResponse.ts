c26d5536e2d0914dc629c73ece91e390fc42f023
Lautauro <aurolaut@protonmail.com>
2024-03-01 14:13:04 -0300

    Fixed "REACT_TO_MESSAGE"

--- src/modules/whatsapp/readResponse.ts
+++ src/modules/whatsapp/readResponse.ts
@@ -11,25 +11,32 @@ export async function readResponse(response: CommandReturn, message: Message): P
     if (response.code === CommandResponse.OK) {
         let _return = null;
 
-        if (response.type !== CommandResponseType.REACT_TO_MESSAGE && response.data.reaction) {
-            reactToMessage(response.data.reaction, message);
+        if (response.data.reaction !== undefined) {
+            if (response.type !== CommandResponseType.REACT_TO_MESSAGE) {
+                reactToMessage(response.data.reaction, message);
+            } else {
+                _return = await reactToMessage(response.data.reaction, message);
+            }
         }
-
+        
         if (response.data.content) {
             switch (response.type) {
                 case CommandResponseType.REPLY_MESSAGE:
                     _return = await replyMessage(response.data.content, message.id, { sendSeen: commandsSettings.sendSeen, ...response.data.options});
+                    break;
                 case CommandResponseType.SEND_MESSAGE:
                     _return = await sendMessage(response.data.content, message.id, { sendSeen: commandsSettings.sendSeen, ...response.data.options});
-                case CommandResponseType.REACT_TO_MESSAGE:
-                    _return = await reactToMessage(response.data.reaction ?? '', message);
+                    break;
             }
         }
+
         botLog('OK RESPONSE:', response);
         if (_return) { return _return; }
+
     } else if (response.code === CommandResponse.ERROR && typeof response.data.content === 'string') {
         botLogError('ERROR RESPONSE:', response);
         await reactToMessage(response.data.reaction ?? '🚫', message);
+        
         return await sendMessage(`🚫 *ERROR* 🚫\n\n${response.data.content}`, message.id, { sendSeen: commandsSettings.sendSeen, ...response.data.options});
     }
 }
