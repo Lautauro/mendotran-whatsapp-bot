862c13ab4f968f078f868612e051eb10e2f2ce91
Lautauro <aurolaut@protonmail.com>
2024-04-02 14:23:51 -0300

    Eliminado log

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,8 @@ export const packageInfo = require('../package.json');
 
 import { startWhatsAppWebClient } from './modules/whatsapp/whatsapp.js'; 
 import fs from 'node:fs';
-import { botLog } from './utils/botLog';
 
 if (!fs.existsSync(`./json/${mendotranSettings.dataFile}`) || process.argv[2] == 'refresh') {
-    botLog('Generando base de datos de Mendotran.');
     require('./modules/mendotran/generateDatabase.js').getMendotranDatabase()
         .then(() => {
             startWhatsAppWebClient();
