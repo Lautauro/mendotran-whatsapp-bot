7e15efd2812e9dd7717573359103bf5f71ad2b55
Lautauro <aurolaut@protonmail.com>
2024-04-02 16:30:26 -0300

    Comando getBusColor añadido.

--- src/modules/mendotran/emojis.ts
+++ src/modules/mendotran/emojis.ts
@@ -1,6 +1,7 @@
-export const METRO_EMOJI: string = '🚉';
+import { BusColor } from "../../ts/types/mendotran";
+export const METRO_EMOJI: BusColor = '🚉';
 
-export const BUS_COLOR_LIST: readonly string[] = [
+export const BUS_COLOR_LIST: readonly BusColor[] = [
     '🔲', // Sin color definido
     '🟥', // 100
     '⬜', // 200
@@ -27,3 +28,16 @@ export const EMOJI_TIME: readonly string[][] = [
     ['🕙', '🕥'], // 10 - 22
     ['🕚', '🕦'], // 11 - 23
 ];
+
+/**
+ * Devuelve el color en emoji de la linea de colectivo.
+ * @param {string} linea - Línea de colectivo
+ * @returns {BusColor}
+ */
+export function getBusColor(linea: string): BusColor {
+    if (+linea >= 100 && +linea < 1000) {
+        return BUS_COLOR_LIST[+String(+linea).charAt(0)];
+    } else {
+        return BUS_COLOR_LIST[0];
+    }
+}
