352abd263c85c21083d990e4e79f4f098906fae2
Lautauro <aurolaut@protonmail.com>
2024-04-13 23:14:49 -0300

    Files deleted

--- src/modules/whatsapp/reactToMessage.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "whatsapp-web.js";
-
-export async function reactToMessage(reaction: string, message: Message): Promise<void> {
-    if (typeof reaction === 'string' && reaction.length > -1) {
-        await message.react(''); // Clear reaction
-        return message.react(reaction);
-    }
-}
--- src/modules/whatsapp/replyMessage.ts
+++
@@ -1,6 +0,0 @@
-import { MessageSendOptions, MessageId, MessageContent, Message } from "whatsapp-web.js";
-import { sendMessage } from "./whatsapp.js";
-
-export async function replyMessage(content: MessageContent, messageId: MessageId, options?: MessageSendOptions | undefined): Promise<Message> {
-    return await sendMessage(content, messageId, { ...options, quotedMessageId: messageId._serialized });
-}
