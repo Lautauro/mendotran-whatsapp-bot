71554dcf1b3015e84f29029f3ce7c8f5cde93dcc
Lautauro <aurolaut@protonmail.com>
2024-04-15 12:28:15 -0300

    Moved printMessage to a new file

--- src/modules/whatsapp/whatsapp.ts
+++ src/modules/whatsapp/whatsapp.ts
@@ -1,12 +1,12 @@
 import { LocalAuth, Client, MessageTypes, Message, MessageId, MessageSendOptions, MessageContent } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
-import { getTimeString } from '../../utils/getTimeString.js';
 import { botLog, botLogError } from '../../utils/botLog.js';
 import { whatsappSettings, commandsSettings, packageInfo } from "../../index.js";
+import { printMessage } from './printMessage.js';
 
 export const wwebClient = new Client({
-    authStrategy: new LocalAuth({ 
-            dataPath: `${whatsappSettings.wwebjsCache}/.wwebjs_auth`,
+    authStrategy: new LocalAuth({
+        dataPath: `${whatsappSettings.wwebjsCache}/.wwebjs_auth`,
     }),
     restartOnAuthFail: true,
     puppeteer: {
@@ -24,6 +24,8 @@ export const wwebClient = new Client({
         path: `${whatsappSettings.wwebjsCache}/.wwebjs_cache`,
     },
 });
+
+let clientStarted: boolean = false;
 
 wwebClient.on('qr', (qr: string) => {
     console.clear();
@@ -46,6 +48,8 @@ wwebClient.on('disconnected', (reason) => {
 });
 
 wwebClient.on('loading_screen', (percent: number) => {
+    if (clientStarted === true) { return; }
+
     let loading_bar: string = '';
     while (loading_bar.length < Math.round(100 * .5)) {
         if (loading_bar.length < Math.round(percent * .5)) {
@@ -56,39 +60,43 @@ wwebClient.on('loading_screen', (percent: number) => {
     }
 
     console.clear();
-    console.log('\n'                                                    +
-        `                 █  ${packageInfo.name.toUpperCase()}  █\n\n`  +
-        '                          ##########\n'                        +
-        '                      #################\n'                     +
-        '                    #####            #####\n'                  +
-        '                  ####                  ####\n'                +
-        '                 ###                      ###\n'               +
-        '                ###     ####               ###\n'              +
-        '               ###     #####                ###\n'             +
-        '               ###     #####                ###\n'             +
-        '               ###      ###                 ###\n'             +
-        '               ###       ####               ###\n'             +
-        '               ###         ####   ####      ###\n'             +
-        '                ###         ##########     ###\n'              +
-        '                 ###            #####     ###\n'               +
-        '                 ###                    ####\n'                +
-        '                ###                  #####\n'                  +
-        '                #######################\n'                     +
-        '               ########   ##########\n\n'                      +
+    console.log('\n' +
+        `                 █  ${packageInfo.name.toUpperCase()}  █\n\n` +
+        '                          ##########\n' +
+        '                      #################\n' +
+        '                    #####            #####\n' +
+        '                  ####                  ####\n' +
+        '                 ###                      ###\n' +
+        '                ###     ####               ###\n' +
+        '               ###     #####                ###\n' +
+        '               ###     #####                ###\n' +
+        '               ###      ###                 ###\n' +
+        '               ###       ####               ###\n' +
+        '               ###         ####   ####      ###\n' +
+        '                ###         ##########     ###\n' +
+        '                 ###            #####     ###\n' +
+        '                 ###                    ####\n' +
+        '                ###                  #####\n' +
+        '                #######################\n' +
+        '               ########   ##########\n\n' +
         `                    Version: ${packageInfo.version}\n`
     );
     console.log(` ${loading_bar} [ ${percent} % ]\n`);
 });
 
 wwebClient.on('ready', () => {
+    if (clientStarted === false) {
+        clientStarted = true;
+    } else { return; }
+
     console.clear();
     botLog('The client is ready.\n');
 
     const startTime = Date.now();
     const commandPath: string = '../commands';
-    let commandExecution  = require(`${commandPath}/commands.js`).commandExecution;
+    let commandExecution = require(`${commandPath}/commands.js`).commandExecution;
     require(`${commandPath}/commandsList.js`);
-    
+
     if (!whatsappSettings.showMessagesInTheTerminal) { botLog('Hidden messages.\n'); }
 
     // Hot-Swap
@@ -97,10 +105,13 @@ wwebClient.on('ready', () => {
             try {
                 delete require.cache[require.resolve(`${commandPath}/commands.js`)];
                 delete require.cache[require.resolve(`${commandPath}/commandsList.js`)];
+                delete require.cache[require.resolve(`./messageSending.js`)];
 
-                commandExecution = require(`${commandPath}/commands.js`).commandExecution; 
+                delete require.cache[require.resolve(`${commandPath}/sendResponses.js`)];
+
+                commandExecution = require(`${commandPath}/commands.js`).commandExecution;
                 require(`${commandPath}/commandsList.js`);
-            } catch(error) {
+            } catch (error) {
                 console.error('Error while clearing cache:', error);
             }
         }, commandsSettings.hotSwappingTimer);
@@ -132,115 +143,17 @@ wwebClient.on('ready', () => {
         // Setting: Show messages in the termianal
         if (whatsappSettings.showMessagesInTheTerminal) { printMessage(message, from); }
 
-        /* Commands */ 
+        /* Commands */
         
         if (commandExecution === undefined) { return; }
         
         // Setting: Ignore commands not coming from admin
         if (whatsappSettings.adminOnly && !message.fromMe) { return; }
 
-        if (message.body.indexOf(commandsSettings.commandPrefix) === 0 && typeof message.body === 'string' && message.type === MessageTypes.TEXT) {          
+        if (message.body.indexOf(commandsSettings.commandPrefix) === 0 && typeof message.body === 'string' && message.type === MessageTypes.TEXT) {
             commandExecution(message);
         }
     });
 });
-
-// Functions
-
-async function printMessage(message: Message, from: string, edited?: boolean): Promise<void> {
-    let terminalText: string = '';
-
-    // Timestamp: Time the message was sent
-    if (whatsappSettings.showTimestamp === true) { 
-        terminalText += `[${getTimeString(message.timestamp * 1000, true, true, true)}] `;
-    }
-
-    // Show contact name if it is booked
-    let userName: string = 'UNKNOWN';
-
-    if (message.fromMe === true) {
-        userName = whatsappSettings.botName;
-    } else {
-        if (whatsappSettings.showContactName) {
-            const contact = await message.getContact();
-            userName = (contact.name === undefined) ? contact.pushname : contact.name;
-        } else if (whatsappSettings.showUserName) {
-            // @ts-ignore            
-            userName = message.rawData.notifyName ?? 'UNDEFINED';
-        } else {
-            userName = 'HIDDEN';
-        }
-    }
-    
-    // Media: Audio, voice message, video, image, etc
-    let messageMedia: string = '';
-
-    // @ts-ignore
-    if (message.isGif) { message.type = 'gif'; }
-    if (message.hasMedia === true || message.type !== MessageTypes.TEXT) {
-        switch (message.type) {
-            case MessageTypes.AUDIO:
-                messageMedia = '🔊 Audio 🔊';
-            break;
-            case MessageTypes.VOICE:
-                messageMedia = '🔊 Voice message 🔊';
-                break;
-            case MessageTypes.STICKER:
-                messageMedia = '💟 Sticker 💟 ';
-                break;
-            case MessageTypes.VIDEO:
-                messageMedia = '📹 Video 📹';
-                break;
-            case MessageTypes.IMAGE:
-                messageMedia = '📷 Image 📷';
-                break;
-            case MessageTypes.DOCUMENT:
-                messageMedia = '📄 Document 📄';
-                break;
-            case MessageTypes.LOCATION:
-                // Prevent raw location data from being printed out
-                message.body = ''; 
-                // @ts-ignore
-                if (message.location.description.length) {
-                    // @ts-ignore
-                    messageMedia = `📍 Location: ${(message.location.description).split('\n').join('. ')} 📍\n`;
-                } else {
-                    messageMedia = '📍 Location 📍';
-                }
-                break;
-            // @ts-ignore
-            case 'gif':
-                messageMedia = '🎞️ GIF 🎞️';
-                break;
-            case MessageTypes.CONTACT_CARD:
-                message.body = '';
-                messageMedia = '📒 Contact card 📒';
-                break;
-            case MessageTypes.POLL_CREATION:
-                messageMedia = '📊 Poll 📊';
-                message.body = `\n"${message.body}":`;
-                for (let i = 0; i < message.pollOptions.length; i++) {
-                    // @ts-ignore
-                    message.body += `\n  ${i+1}) - ${message.pollOptions[i].name}`;
-                }
-                break;
-            default:
-                messageMedia = `${message.type}`;
-                break;
-        }
-        if (message.body.length > 0) { messageMedia += ': '; }
-    }
-
-    // Setting: Show phone number
-    if (whatsappSettings.showPhoneNumber === true) { terminalText += `${from}`; }
-    // User name
-    terminalText += `| <${userName}> `;
-    // Media
-    if (messageMedia) { terminalText += messageMedia; }
-    // Edited
-    if (edited) { terminalText += '[✍️ EDITED ✍️] '; }
-
-    console.log(`${terminalText}${message.body}`);
-}
 
 export function startWhatsAppWebClient(): void { wwebClient.initialize(); }
