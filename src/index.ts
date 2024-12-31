7b10e94e191c36812f23520e3bd50d4d32b6e0ed
Lautauro <aurolaut@protonmail.com>
2024-12-31 15:02:15 -0300

    Informar cuando la base de datos no ha sido encontrada

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 export const whatsappSettings = require('../config/whatsapp.json');
 export const commandsSettings = require('../config/commands.json');
 export const mendotranSettings = require('../config/mendotran.json');
-export const packageInfo = require('../package.json');
 export const commandsPrefix = commandsSettings.commandPrefix ?? '';
+export const packageInfo = require('../package.json');
+
+import { botLogError } from './utils/botLog.js'
 
 import { startWhatsAppWebClient } from './modules/whatsapp/client.js'; 
 import fs from 'node:fs';
 
-if (!fs.existsSync(`./json/${mendotranSettings.dataFile}`) || process.argv[2] == 'refresh') {
-    require('./modules/mendotran/generateDatabase.js').getMendotranDatabase()
+// Verificar si la base de datos existe
+if (!fs.existsSync(`./json/${mendotranSettings.dataFile}`) || process.argv.includes('refresh')) {
+    // Generar base de datos
+    if (!process.argv.includes('refresh')) {
+        botLogError(`No se ha encontrado el archivo de la base de datos en './json/${mendotranSettings.dataFile}'. `
+            + `Se procederá a generar la base de datos.`
+        );
+    }
+    
+    require('./modules/mendotran/generateDatabase.js')
+        .getMendotranDatabase()
         .then(() => {
             startWhatsAppWebClient();
         });
